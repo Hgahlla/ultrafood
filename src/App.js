678aeff449678f0cdb1fb915eb77f30f6efb5063
Hgahlla <harmohangahlla@gmail.com>
2022-03-08 12:16:48 -0600

    Added HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Header from "./components/Header";
-
+import HomePage from "./pages/HomePage";
 const App = () => {
   return (
     <>
       <Header />
+      <HomePage />
     </>
   );
 };
