3c564df37ae49bbc4ba64e5977e992d0886afd4a
Hgahlla <harmohangahlla@gmail.com>
2022-03-08 16:55:23 -0600

    Added HowItWorks component to the HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "../components/Hero";
 import FeaturedIn from "../components/FeaturedIn";
+import HowItWorks from "../components/HowItWorks";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
       <FeaturedIn />
+      <HowItWorks />
     </main>
   );
 };
