9b4cdc2eb99bdeb886a7fd3a2966774bb0f7c8eb
Hgahlla <harmohangahlla@gmail.com>
2022-03-08 17:12:45 -0600

    Added Meals component to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import FeaturedIn from "../components/FeaturedIn";
 import HowItWorks from "../components/HowItWorks";
+import Meals from "../components/Meals";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Hero />
       <FeaturedIn />
       <HowItWorks />
+      <Meals />
     </main>
   );
 };
